58833525b52eb7fb1bd12c1658f85d8d55d89d2a
brunogabrielpk <brunogabrielpk@gmail.com>
2025-06-03 22:05:07 -0400

    c3

--- main.go
+++ main.go
@@ -1,25 +1,31 @@
-package main 
+package main
 
 import "fmt"
 
 /* Itens do Hot-dog
  Salsicha, pão e molho [obrigatórios]
  Ervilha, Milho, bacon, calabresa, bacon, purê
- Valores: 
-	Pão = R$ 1,00	
-	Molho = R$ 0,50	
-	Salcisha = R$ 1,00	
-	Ervilha = R$ 0,25	
+ Valores:
+	Pão = R$ 1,00
+	Molho = R$ 0,50
+	Salcisha = R$ 1,00
+	Ervilha = R$ 0,25
 	Milho = R$ 0,25
-	Bacon = R$ 2,00	
-	Calabresa = R$ 1,50	
-	Purê = R$ 1,00	
+	Bacon = R$ 2,00
+	Calabresa = R$ 1,50
+	Purê = R$ 1,00
 	****
 	Margem de lucro inicial : 200%
- */
+*/
 
 func main() {
-var entradaUser int
+	var opcaoUserMenu int
+	var userConfirma string
+	// sn := false
+	margemLucro := 2.0
+	itensHD := [8]string{"Pão", "Molho", "Salsicha", "Ervilha", "Milho", "Bacon", "Calabresa", "Purê"}
+	//itensHDvalor := [8]float64{1.0, 0.5, 1.0, 0.25, 0.25, 2.0, 1.5, 1.0}
+	itensHDconfirma := [8]bool{true, true, true, false, false, false, false, false}
 
 	// Mostrar o menu para o usuários
 	for {
@@ -28,12 +34,29 @@ var entradaUser int
 		fmt.Println("[1] - Pedir um Hot-Dog")
 		fmt.Println("[2] - Definir a margem de lucro")
 		fmt.Println("[3] - Sair")
-		fmt.Scanln(&entradaUser)
+		fmt.Scanln(&opcaoUserMenu)
 
-		if entradaUser == 3 {
+		if opcaoUserMenu == 1 {
+			fmt.Println("Vamos definir os adicionais de seu Hot-Dog")
+			// Verifica quais adicionais o usuário deseja
+			for pos, item := range itensHD {
+				if pos >= 3 {
+					fmt.Printf("Deseja adicionar %v?\n", item)
+					fmt.Scanln(&userConfirma)
+					if userConfirma == "s" || userConfirma == "S" {
+						itensHDconfirma[pos] = true
+					} else {
+						itensHDconfirma[pos] = false
+					}
+				}
+			}
+		} else if opcaoUserMenu == 2 {
+			fmt.Println("Digite a nova margem de lucro (apenas números)")
+			fmt.Scanln(&margemLucro)
+		} else if opcaoUserMenu == 3 {
 			break
 		} else {
-			fmt.Println("Não foi dessa vez")
+			fmt.Println("Escolha uma opção válida")
 		}
 	}
 }
